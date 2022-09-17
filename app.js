dcdf6eb04f26460c1c0a4eccf5736b7ecceae9bc
Andrey <105484100+Sixt1e@users.noreply.github.com>
2022-09-17 17:35:03 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 $(document).ready(function() {
-$('#registration').validate({
+$('#registration').validate ({
     rules: {
         email: {
             required: true,
             email: true
+        },
+        password: {
+            required: true,
+            rangelength: [10,16]
+        },
+        confirm_password: {
+            equalTo: '#password'
         }
     },
+
+    messages: {
+        email: {
+            required: 'Please enter your email address.',
+            email: 'Incorrect email address.'
+        },
+    password: {
+        required: 'Enter password.',
+        rangelength: 'Password must be between 10 and 16 characters.'
+    },
+    confirm_password: {
+        equalTo: 'Passwords do not match.'
+    }
+    }
 })
 });
